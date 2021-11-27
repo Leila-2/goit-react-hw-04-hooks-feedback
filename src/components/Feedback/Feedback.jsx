cf060c2c4747eaaab2d04b31a2474cb6e9fd6da8
Leila-2 <liliya.yavorskaya17@gmail.com>
2021-11-27 14:58:12 +0200

    рефакторит код

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,65 +1,129 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { FeedbackElems } from "../FeedbackOpt/FeedbackElems";
 import data from "../data/feedbackData.json";
 import Statistic from "../Statistics/Statistic";
 import { Section } from "../Section/Section";
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+
+export default function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleIncrement = (e) => {
+    //const key = e.target.id;
+    // console.log(key);
+    switch (e) {
+      case "good":
+        setGood((state) => state + 1);
+        break;
+      case "neutral":
+        setNeutral((state) => state + 1);
+        break;
+      case "bad":
+        setBad((state) => state + 1);
+        break;
+      default:
+        break;
+    }
+  };
+  // const handleGoodIncrement = () => {
+  //   setGood((state) => state + 1);
+  // };
+  // const handleNeutralIncrement = () => {
+  //   setNeutral((state) => state + 1);
+  // };
+  // const handleBadIncrement = () => {
+  //   setBad((state) => state + 1);
+  // };
+
+  const totalClicks = () => {
+    const total = good + neutral + bad;
+    return total;
   };
 
-  handleIncrement = (option) => {
-    this.setState({
-      [option]: this.state[option] + 1,
-    });
-    //console.log(this.state);
-  };
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    const res = good + neutral + bad;
-    //console.log(res);
-    return res;
-  }
-
-  countPositiveFeedbackPercentage(total) {
-    const { good } = this.state;
-    const result = Math.round((good / total) * 100);
+  function countPercentage() {
+    const result = Math.round((good / totalClicks()) * 100);
     return result;
   }
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalNum = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage(totalNum);
-    //console.log(totalNum);
-    return (
-      <>
-        <Section
-          title="Please leave your feedback"
-          child={
-            <FeedbackElems
-              options={data}
-              incrementFunc={this.handleIncrement}
-            />
-          }
-        ></Section>
-        <Section
-          title="Statistics"
-          child={
-            <Statistic
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalNum}
-              percentage={percentage}
-            />
-          }
-        ></Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <Section
+        title="Please leave your feedback"
+        child={<FeedbackElems options={data} func={handleIncrement} />}
+      ></Section>
+      <Section
+        title="Statistics"
+        child={
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalClicks()}
+            percentage={countPercentage()}
+          />
+        }
+      ></Section>
+    </>
+  );
 }
 
-export default Feedback;
+// class Feedback extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handleIncrement = (option) => {
+//     this.setState({
+//       [option]: this.state[option] + 1,
+//     });
+//     //console.log(this.state);
+//   };
+//   countTotalFeedback() {
+//     const { good, neutral, bad } = this.state;
+//     const res = good + neutral + bad;
+//     //console.log(res);
+//     return res;
+//   }
+
+//   countPositiveFeedbackPercentage(total) {
+//     const { good } = this.state;
+//     const result = Math.round((good / total) * 100);
+//     return result;
+//   }
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const totalNum = this.countTotalFeedback();
+//     const percentage = this.countPositiveFeedbackPercentage(totalNum);
+//     //console.log(totalNum);
+//     return (
+//       <>
+//         <Section
+//           title="Please leave your feedback"
+//           child={
+//             <FeedbackElems
+//               options={data}
+//               incrementFunc={this.handleIncrement}
+//             />
+//           }
+//         ></Section>
+//         <Section
+//           title="Statistics"
+//           child={
+//             <Statistic
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={totalNum}
+//               percentage={percentage}
+//             />
+//           }
+//         ></Section>
+//       </>
+//     );
+//   }
+// }
+
+// export default Feedback;
--- src/components/FeedbackOpt/FeedbackElems.jsx
+++ src/components/FeedbackOpt/FeedbackElems.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import s from "../FeedbackOpt/Feedback.module.css";
 
 //import PropTypes from "prop-types";
-const FeedbackElems = ({ options, incrementFunc }) => (
+const FeedbackElems = ({ options, func }) => (
   <ul className={s.list}>
     {options.map(({ id, value }) => {
       return (
-        <li
-          className={s.listItem}
-          key={id}
-          onClick={() => incrementFunc(value)}
-        >
+        <li className={s.listItem} key={id} onClick={() => func(value)}>
           <button type="button">{value}</button>
         </li>
       );
